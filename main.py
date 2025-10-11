f1771360b9bd1c99164c23e8c139aca0bf5e2f00
ekhadley <ekhadley@gmail.com>
2025-10-10 21:06:00 -0500

    flask + no nginx

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from website!")
+import sys
+from pathlib import Path
+from flask_socketio import SocketIO
+from flask import Flask, render_template
 
+app = Flask(
+    __name__,
+    template_folder="website/templates",
+    static_folder="website/static",
+)
+socket = SocketIO(app, cors_allowed_origins="*")
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    main()
+    socket.run(app, host="localhost", port=80)
